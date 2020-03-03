8c276296c2d88d311048d0af8c598b811dbcbfc9
David Omid <davidomid@hotmail.com>
2020-03-03 23:54:17 +0000

    Updated slides and example code

---
+++ AsyncAwaitQuiz/Examples/SynchronousOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AsyncAwaitQuiz.Examples
+{
+    public class SynchronousOperations
+    {
+        public void DoLotsOfWork()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                DoAThing(i);
+            }
+        }
+
+        public void DoAThing(int number)
+        {
+            // Some sort of work here
+            Console.WriteLine($"Doing a thing with number {number}");
+        }
+
+        public int SynchronousCalculation()
+        {
+            int resultA = A();
+            int resultB = B();
+            int resultC = C();
+            int resultD = D();
+            return resultA + resultB + resultC + resultD;
+        }
+
+        public void DoLongRunningStuff()
+        {
+            // Simulating a synchronous operation which takes a long time
+            Thread.Sleep(TimeSpan.FromMinutes(1));
+        }
+
+        private static int A()
+        {
+            return 1;
+        }
+
+        private static int B()
+        {
+            return 2;
+        }
+
+        private static int C()
+        {
+            return 3;
+        }
+
+        private static int D()
+        {
+            return 4;
+        }
+    }
+}
---
+++ Tests/Examples/SynchronousOperationsTests.cs
@@ -0,0 +1,26 @@
+﻿using AsyncAwaitQuiz.Examples;
+using NUnit.Framework;
+
+namespace Tests.Examples
+{
+    public class SynchronousOperationsTests
+    {
+        [Test]
+        public static void Test_SynchronousCalculation()
+        {
+            new SynchronousOperations().SynchronousCalculation();
+        }
+
+        [Test]
+        public static void Test_DoLotsOfWork()
+        {
+            new SynchronousOperations().DoLotsOfWork();
+        }
+
+        [Test]
+        public static void Test_DoLongRunningStuff()
+        {
+            new SynchronousOperations().DoLongRunningStuff();
+        }
+    }
+}
