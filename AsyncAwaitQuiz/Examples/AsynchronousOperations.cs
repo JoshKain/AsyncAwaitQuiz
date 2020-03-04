6c9b64e77bdc2d351f9efdd82331a8a809b565d2
David Omid <davidomid@hotmail.com>
2020-03-04 20:20:50 +0000

    Improved slides and added some asynchronous operation examples

---
+++ AsyncAwaitQuiz/Examples/AsynchronousOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitQuiz.Examples
+{
+    public class AsynchronousOperations
+    {
+        public Task<byte[]> DownloadImageAsync(string url)
+        {
+            var client = new HttpClient();
+            return client.GetByteArrayAsync(url);
+        }
+
+        public async Task SaveImageAsync(byte[] bytes, string imagePath)
+        {
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await fileStream.WriteAsync(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
