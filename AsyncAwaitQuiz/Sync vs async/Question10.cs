8f7f75127dc53935f7b31b3b77ccb342f8af9c4b
David Omid <davidomid@hotmail.com>
2020-03-05 00:44:29 +0000

    Modified test question 10 and updated slides

--- AsyncAwaitQuiz/Sync vs async/Question10.cs
+++ AsyncAwaitQuiz/Sync vs async/Question10.cs
@@ -20,29 +20,29 @@ namespace AsyncAwaitQuiz.Sync_vs_async
 
         public static async Task Method1Async()
         {
-            Task operation1ResultTask1 = Operation1ResultAsync();
-            Task operation1ResultTask2 = Operation1ResultAsync();
-            Task operation1ResultTask3 = Operation1ResultAsync();
+            Task operation1ResultTask1 = OperationWithResultAsync();
+            Task operation1ResultTask2 = OperationWithResultAsync();
+            Task operation1ResultTask3 = OperationWithResultAsync();
             await operation1ResultTask1;
             await operation1ResultTask2;
             await operation1ResultTask3;
         }
         public static async Task Method2Async()
         {
-            Task operation1AwaitTask1 = Operation1AwaitAsync();
-            Task operation1AwaitTask2 = Operation1AwaitAsync();
-            Task operation1AwaitTask3 = Operation1AwaitAsync();
+            Task operation1AwaitTask1 = OperationWithAwaitAsync();
+            Task operation1AwaitTask2 = OperationWithAwaitAsync();
+            Task operation1AwaitTask3 = OperationWithAwaitAsync();
             await operation1AwaitTask1;
             await operation1AwaitTask2;
             await operation1AwaitTask3;
         }
-        private static async Task Operation1AwaitAsync()
+        private static async Task OperationWithAwaitAsync()
         {
             HttpClient httpClient = new HttpClient();
             HttpResponseMessage httpResponseMessage = await httpClient
                 .GetAsync(new Uri("http://www.deelay.me/5000/http://www.bbc.com"));
         }
-        private static Task Operation1ResultAsync()
+        private static Task OperationWithResultAsync()
         {
             HttpClient httpClient = new HttpClient();
             HttpResponseMessage httpResponseMessage = httpClient
