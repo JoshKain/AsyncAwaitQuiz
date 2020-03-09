477ec31e58e56d2b32c5bd7f9a3f0e3ac681ee21
David Omid <davidomid@hotmail.com>
2020-03-09 23:14:42 +0000

    Updated slides and code examples for synchronous operations

---
+++ Tests/Examples/BlockingOperations.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using AsyncAwaitQuiz.Examples;
+using NUnit.Framework;
+
+namespace Tests.Examples
+{
+    public class BlockingOperationsTests
+    {
+        [Test]
+        public static void Test_Operation()
+        {
+            new BlockingOperations().Operation();
+        }
+
+        [Test]
+        public static async Task Test_OperationAsync()
+        {
+            await new BlockingOperations().OperationAsync();
+        }
+    }
+}
